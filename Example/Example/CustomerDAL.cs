9663cd407733d9efc752b9fe80353ff030cd5b84
BenHall <ben@benhall.me.uk>
2010-06-10 00:36:00 +0100

    Database duck typing example

---
+++ Example/Example/CustomerDAL.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+
+namespace Example
+{
+    public class CustomerDAL
+    {
+        public DbConnection Database { get; set; }
+
+        public CustomerDAL(DbConnection database)
+        {
+            Database = database;
+        }
+
+        public void SendReminders()
+        {
+            DbCommand dbCommand = Database.CreateCommand();
+            dbCommand.CommandText = "Do something...";
+            dbCommand.ExecuteNonQuery();
+        }
+    }
+}
